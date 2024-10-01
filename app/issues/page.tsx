33ca649bb87b15d792d19b3f04d7b9fe77d0ec64
Bernd Ullmann <info@berndullmann.de>
2024-10-01 14:06:02 +0200

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Button, Flex } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Flex gap="3">
+        <Button color="indigo" variant="soft">
+          Edit profile
+        </Button>
+        <Button color="cyan" variant="soft">
+          Edit profile
+        </Button>
+        <Button color="orange" variant="soft">
+          Edit profile
+        </Button>
+        <Button color="crimson" variant="soft">
+          Edit profile
+        </Button>
+      </Flex>
+    </div>
+  );
 };
 
 export default IssuesPage;
