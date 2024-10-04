8189e5129c36ffe0f294389899d2d21b6ba03f99
Bernd Ullmann <info@berndullmann.de>
2024-10-04 12:01:24 +0200

    implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,23 +2,28 @@
 
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField, Text } from "@radix-ui/themes";
 import { MagnifyingGlassIcon, InfoCircledIcon } from "@radix-ui/react-icons";
 import { useForm, Controller } from "react-hook-form";
 import { useState } from "react";
-import { title } from "process";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { error } from "console";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
   return (
     <div className="max-w-xl">
@@ -47,6 +52,11 @@ const NewIssuePage = () => {
             <MagnifyingGlassIcon height="16" width="16" />
           </TextField.Slot>
         </TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           render={({ field }) => (
             <SimpleMDE placeholder="Reply to comment…" {...field} />
@@ -54,7 +64,11 @@ const NewIssuePage = () => {
           name="description"
           control={control}
         />
-
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         <Button color="indigo" variant="soft">
           Submit new{" "}
         </Button>
