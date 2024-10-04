454d5876c79ae61fa77fca17af32fe80aaf74d6b
Bernd Ullmann <info@berndullmann.de>
2024-10-04 11:27:51 +0200

    handling errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,12 +2,14 @@
 
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { Button, TextField } from "@radix-ui/themes";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import { MagnifyingGlassIcon, InfoCircledIcon } from "@radix-ui/react-icons";
 import { useForm, Controller } from "react-hook-form";
+import { useState } from "react";
 import { title } from "process";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { error } from "console";
 
 interface IssueForm {
   title: string;
@@ -17,31 +19,47 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root placeholder="Search the docs…" {...register("title")}>
-        <TextField.Slot>
-          <MagnifyingGlassIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
-      <Controller
-        render={({ field }) => (
-          <SimpleMDE placeholder="Reply to comment…" {...field} />
-        )}
-        name="description"
-        control={control}
-      />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon>
+            <InfoCircledIcon />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
 
-      <Button color="indigo" variant="soft">
-        Submit new{" "}
-      </Button>
-    </form>
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occurred. Please try again later.");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Search the docs…" {...register("title")}>
+          <TextField.Slot>
+            <MagnifyingGlassIcon height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
+        <Controller
+          render={({ field }) => (
+            <SimpleMDE placeholder="Reply to comment…" {...field} />
+          )}
+          name="description"
+          control={control}
+        />
+
+        <Button color="indigo" variant="soft">
+          Submit new{" "}
+        </Button>
+      </form>
+    </div>
   );
 };
 
